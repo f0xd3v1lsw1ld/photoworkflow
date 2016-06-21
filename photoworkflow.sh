71b5738c88ae8b7027bb75b121bcb69dd13173bf
f0xd3v1lsw1ld <f0xd3v1lsw1ld@gmail.com>
2016-06-21 22:12:44 +0200

    better output for user

--- photoworkflow.sh
+++ photoworkflow.sh
@@ -49,9 +49,11 @@ for ext in jpg JPG CR2 cr2; do
           pushd $home_dir &>/dev/null
 
           #rename the newfiles with exiftool
+          echo "rename new images"
           exiftool -m "-filename<DateTimeOriginal" -d IMG_%Y-%m-%d-%H_%M_%S%%-c.%%le -progress *.$ext
                   
           #move newfiles with exiftool in your directory structure, change file extension to lower case
+          echo "move new images to: "$working_dir
           exiftool -m '-Directory<DateTimeOriginal' -d "$working_dir/%Y/%Y-%m-%d" -progress *.${ext,,} 2>/dev/null
           rm $home_dir/newfile
           popd &>/dev/null
@@ -60,10 +62,7 @@ for ext in jpg JPG CR2 cr2; do
           # there are files with this extension, but this files were already imported
           echo "no new images to proceed"
         fi
-   else
-     # there are no files with this extension in the directory
-     echo "no "$ext " files to proceed "
-   fi
+  fi
 done
 
 #cleanup
